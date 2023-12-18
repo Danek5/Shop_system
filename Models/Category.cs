ac5ad2c7037615131fec4d8ce25b84dc744a582a
Danek5 <danek.trombik@gmail.com>
2023-12-18 18:04:40 +0100

    final changes

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,11 @@
-﻿namespace Shop_system.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop_system.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
     }
