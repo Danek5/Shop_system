ab19fcb814fe27b7746e26661133a3a25e8e1eb2
Danek5 <danek.trombik@gmail.com>
2023-12-18 16:28:53 +0100

    continuing in development

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop_system.Models;
+
+namespace Shop_system.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categories = CategoryRepository.GetCategories();
+            return View(categories);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            var category = CategoryRepository.GetCategoryById(id.HasValue?id.Value:0);
+
+            return View(category);
+        }
+    }
+}
